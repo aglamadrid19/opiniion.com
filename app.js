02af12cec0ec816e3834daf3b74d3886da49fc39
aglamadrid19 <aglamadrid19@gmail.com>
2024-10-03 03:18:48 +0000

    added error handling to server startup

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(express.json());
 // Use the user router for /users endpoint
 app.use('/', userRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log("Server is Successfully Running, and App is listening on port " + PORT);
+  else
+    console.log("Error occurred, server can't start", error);
 });
